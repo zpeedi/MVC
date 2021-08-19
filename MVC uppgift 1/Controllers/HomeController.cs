767077a3e717f5824b5012cb9dd313c25ef8beb4
Magnus Larsson <zpeedi@hotmail.com>
2021-08-19 12:25:57 +0200

    Add project files.

---
+++ MVC uppgift 1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_uppgift_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
